41c3357162867d23d162dcfcb308cf11d9f342ed
xylophone21 <xylophone21@gmail.com>
2014-09-10 17:40:35 +0800

    rename libs->binder and sbinder->StandaloneBinder

---
+++ build/cmakes/FindStandaloneBinder.cmake
@@ -0,0 +1,15 @@
+FIND_PATH(StandaloneBinder_INCLUDE_DIR binder.h ${PROJECT_SOURCE_DIR}/binder/include/)
+
+IF (StandaloneBinder_INCLUDE_DIR)
+    SET(StandaloneBinder_FOUND TRUE)
+ENDIF (StandaloneBinder_INCLUDE_DIR)
+
+IF (StandaloneBinder_FOUND)
+    IF (NOT StandaloneBinder_FIND_QUIETLY)
+        MESSAGE(STATUS "Found StandaloneBinder: ${StandaloneBinder_INCLUDE_DIR}") 
+    ENDIF (NOT StandaloneBinder_FIND_QUIETLY)
+ELSE (StandaloneBinder_FOUND)
+    IF (StandaloneBinder_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find StandaloneBinder")
+    ENDIF (StandaloneBinder_FIND_REQUIRED)
+ENDIF (StandaloneBinder_FOUND)
